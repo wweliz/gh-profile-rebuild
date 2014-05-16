4f6690c07a4128d6aa84061f830b55eff140606d
Whitney Williams <wweliz@gmail.com>
2014-05-16 10:31:41 -0400

    first commit - add keys to run

---
+++ app/scripts/main.js
@@ -0,0 +1,32 @@
+// Repositories ////////////////////////////////////////////////////
+
+var showRepos = _.template($('.showRepos').text());
+
+// passing repodata provided by the getJSON through the function
+function renderRepos (repodata) {
+  //forEaching over the repodata
+  repodata.forEach(function(repo){
+    // writes the text into var rendered
+    var rendered = showRepos(repo);
+    //puts the string into the repo-tab-content div
+    $('.repo-tab-content').prepend(rendered);
+  });
+}
+
+//fetches the info from my github repo page, once it has all the data, it makes the data available for the renderRepos function
+$.getJSON('https://api.github.com/users/wweliz/repos').done(function(data){
+  renderRepos(data);
+});
+
+// Sidebar Info ////////////////////////////////////////////////////
+
+var showSidebar = _.template($('.showSidebar').text());
+
+function renderSidebar (data, destination){
+    var rendered = showSidebar(data);
+    destination.prepend(rendered);
+  }
+ 
+$.getJSON('https://api.github.com/users/wweliz').done(function(data){
+  renderSidebar(data, $('.sidebar'));
+});
